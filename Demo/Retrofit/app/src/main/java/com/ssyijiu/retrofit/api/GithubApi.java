7f553a82329b4a4869fae888e4327ed49e01990d
ssyijiu <ssyijiu@126.com>
2016-10-17 19:17:22 +0800

    update Retrofit

--- Demo/Retrofit/app/src/main/java/com/ssyijiu/retrofit/api/GithubApi.java
+++
@@ -1,8 +0,0 @@
-package com.ssyijiu.retrofit.api;
-
-/**
- * Created by lxm on 2016/10/13.
- */
-public interface GithubApi {
-
-}
--- Demo/Retrofit/app/src/main/java/com/ssyijiu/retrofit/helper/RetrofitHelper.java
+++ Demo/Retrofit/app/src/main/java/com/ssyijiu/retrofit/helper/RetrofitHelper.java
@@ -1,7 +1,5 @@
 package com.ssyijiu.retrofit.helper;
 
-import com.ssyijiu.retrofit.api.GankApi;
-
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
