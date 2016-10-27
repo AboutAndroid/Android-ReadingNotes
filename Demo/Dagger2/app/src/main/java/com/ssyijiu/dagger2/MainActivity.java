cccb2ecce18eefa438b683fb4b41882e70a3112a
ssyijiu <ssyijiu@126.com>
2016-10-27 20:05:00 +0800

    add Demo/Dagger2

---
+++ Demo/Dagger2/app/src/main/java/com/ssyijiu/dagger2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ssyijiu.dagger2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import junit.framework.Test;
+
+import javax.inject.Inject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
