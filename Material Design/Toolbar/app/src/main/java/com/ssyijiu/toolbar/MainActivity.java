dc518ed2b9967d3cb034cafb47156e130186a168
ssyijiu <ssyijiu@126.com>
2016-11-08 15:55:16 +0800

    update Demo and Material Design

--- Material Design/Toolbar/app/src/main/java/com/ssyijiu/toolbar/MainActivity.java
+++ Material Design/Toolbar/app/src/main/java/com/ssyijiu/toolbar/MainActivity.java
@@ -1,16 +1,12 @@
 package com.ssyijiu.toolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.WindowDecorActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.Toast;
-
-import static com.ssyijiu.toolbar.R.id.toolbar;
 
 
 /**
