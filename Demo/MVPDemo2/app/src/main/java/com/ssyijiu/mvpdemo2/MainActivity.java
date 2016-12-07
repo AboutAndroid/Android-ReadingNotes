f995c66bc8e2e019e4b478beb4809259f8542fe7
ssyijiu <lxmyijiu@163.com>
2016-12-07 23:54:11 +0800

    add Demo/Vector

--- Demo/MVPDemo2/app/src/main/java/com/ssyijiu/mvpdemo2/MainActivity.java
+++ Demo/MVPDemo2/app/src/main/java/com/ssyijiu/mvpdemo2/MainActivity.java
@@ -1,14 +1,12 @@
 package com.ssyijiu.mvpdemo2;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.lzh.nonview.router.Router;
 import com.ssyijiu.mvpdemo2.router.RouteUrl;
 import com.ssyijiu.mvpdemo2.router.Routes;
-import com.ssyijiu.mvpdemo2.ui.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ Demo/Vector/app/src/main/java/com/ssyijiu/vector/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ssyijiu.vector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * http://blog.csdn.net/eclipsexys/article/details/51838119
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
